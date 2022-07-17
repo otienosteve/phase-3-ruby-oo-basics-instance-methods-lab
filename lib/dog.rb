47f90887d853e6932f88f675d41e800fee3a3836
otienosteve <steveotieno701@gmail.com>
2022-07-17 14:09:56 +0300

    commit tree fb205f269d5333824280ad7ca4f6884334e94e3d

---
+++ lib/dog.rb
@@ -0,0 +1,11 @@
+class Dog 
+@bark
+@sit
+def bark 
+    puts "Woof!"
+end 
+def sit
+   puts "The Dog is sitting"
+end
+
+end 
---
+++ lib/person.rb
@@ -0,0 +1,14 @@
+class Person 
+@walk
+@talk
+    def talk
+puts "Hello World!"
+"Hello World!"
+end 
+
+def walk 
+puts "The Person is walking"
+"The Person is walking"
+end 
+
+end 
